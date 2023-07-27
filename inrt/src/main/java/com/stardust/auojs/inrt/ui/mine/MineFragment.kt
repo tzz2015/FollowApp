f3d234803edc3cf372326cdf51686ca1f965bc14
tzz2015 <1374329135@qq.com>
2023-07-27 22:29:52 +0800

    显示网页

--- inrt/src/main/java/com/stardust/auojs/inrt/ui/mine/MineFragment.kt
+++ inrt/src/main/java/com/stardust/auojs/inrt/ui/mine/MineFragment.kt
@@ -1,18 +1,47 @@
 package com.stardust.auojs.inrt.ui.mine
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.mind.lib.base.BaseFragment
 import com.mind.lib.base.ViewModelConfig
 import org.autojs.autoxjs.inrt.R
-import org.autojs.autoxjs.inrt.databinding.FragmentNotificationsBinding
+import org.autojs.autoxjs.inrt.databinding.FragmentMineBinding
 
-class MineFragment : BaseFragment<MineViewModel, FragmentNotificationsBinding>() {
+class MineFragment : BaseFragment<MineViewModel, FragmentMineBinding>() {
 
     override val viewModelConfig: ViewModelConfig
-        get() = ViewModelConfig(R.layout.fragment_notifications)
+        get() = ViewModelConfig(R.layout.fragment_mine)
 
 
     override fun init(savedInstanceState: Bundle?) {
         viewModel.getAnnouncementList()
+        setHeaderImage()
+        setAnimator()
+    }
+
+    private fun setAnimator() {
+        // 加载动画
+        val animatorSet: AnimatorSet =
+            AnimatorInflater.loadAnimator(requireContext(), R.animator.anim_up_down) as AnimatorSet
+        animatorSet.setTarget(bind.tvLogin)
+        // 设置循环播放
+        val objectAnimatorUp = animatorSet.childAnimations[0] as ObjectAnimator
+        objectAnimatorUp.repeatMode = ObjectAnimator.REVERSE
+        objectAnimatorUp.repeatCount = ObjectAnimator.INFINITE
+
+        val objectAnimatorDown = animatorSet.childAnimations[1] as ObjectAnimator
+        objectAnimatorDown.repeatMode = ObjectAnimator.REVERSE
+        objectAnimatorDown.repeatCount = ObjectAnimator.INFINITE
+        // 开始动画
+        animatorSet.start()
+    }
+
+    private fun setHeaderImage() {
+        Glide.with(requireContext()).load(viewModel.getHeadImage())
+            .apply(RequestOptions.circleCropTransform()).into(bind.ivHeader)
     }
 }
--- inrt/src/main/java/com/stardust/auojs/inrt/ui/mine/MineViewModel.kt
+++ inrt/src/main/java/com/stardust/auojs/inrt/ui/mine/MineViewModel.kt
@@ -6,12 +6,33 @@ import com.mind.data.http.ApiClient
 import com.mind.lib.base.BaseViewModel
 import com.mind.lib.base.ViewModelEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import org.autojs.autoxjs.inrt.R
 import javax.inject.Inject
+import kotlin.random.Random
 
 @HiltViewModel
 class MineViewModel @Inject constructor() : BaseViewModel() {
 
     val announcementList by lazy { ViewModelEvent<MutableList<AnnouncementModel>>() }
+    private val imageArray = arrayOf(
+        R.mipmap.icon_header_1_1,
+        R.mipmap.icon_header_1_2,
+        R.mipmap.icon_header_1_3,
+        R.mipmap.icon_header_1_4,
+        R.mipmap.icon_header_1_5,
+        R.mipmap.icon_header_1_6,
+        R.mipmap.icon_header_1_7,
+        R.mipmap.icon_header_1_8,
+        R.mipmap.icon_header_1_9,
+        R.mipmap.icon_header_1_10,
+        R.mipmap.icon_header_1_11,
+        R.mipmap.icon_header_1_12,
+        R.mipmap.icon_header_1_13,
+        R.mipmap.icon_header_1_14,
+        R.mipmap.icon_header_1_15,
+        R.mipmap.icon_header_1_16,
+        R.mipmap.icon_header_1_17,
+    )
 
 
     fun getAnnouncementList() {
@@ -25,5 +46,10 @@ class MineViewModel @Inject constructor() : BaseViewModel() {
             },
             isShowDialog = false
         )
+    }
+
+    fun getHeadImage(): Int {
+        val randomInt = Random.nextInt(0, imageArray.size)
+        return imageArray[randomInt]
     }
 }
