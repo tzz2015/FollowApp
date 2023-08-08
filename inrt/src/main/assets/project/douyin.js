720ba7ec826db1329bc8e4ea9e98c0ca96b9cb40
tzz2015 <1374329135@qq.com>
2023-08-08 23:29:36 +0800

    脚本编写

--- inrt/src/main/assets/project/douyin.js
+++ inrt/src/main/assets/project/douyin.js
@@ -11,8 +11,8 @@ var host = readFile("host.txt")
 var editText = className("EditText").findOne()
 if (editText != null) {
     console.log("找到输入框")
-    console.log(token)
-    console.log(accountText)
+    // console.log(token)
+    // console.log(accountText)
     toFollow()
 } else {
     console.log("未找打输入框，请打开搜索界面")
@@ -25,26 +25,100 @@ function toFollow() {
     try {
         var jsonArray = JSON.parse(accountText);
         for (var i = 0; i < jsonArray.length; i++) {
-            var account = jsonArray[i];
-            check(account)
+            var followAccount = jsonArray[i];
+            if (check(followAccount)) {
+                doFollow(followAccount)
+            }
         }
     } catch (e) {
         log("JSON 解析出错：" + e);
     }
 }
 
+function doFollow(followAccount) {
+    editText.setText(followAccount.account);
+    var searchBtn = desc("搜索").findOne()
+    if (searchBtn != null) {
+        console.log("找到搜索按钮")
+        clickView(searchBtn)
+        console.log("点击搜索用户:" + followAccount.account)
+        clickFollowBtn(followAccount)
+    } else {
+        console.log("找不到搜索按钮")
+    }
+
+}
+
+function clickFollowBtn(followAccount) {
+    var followBtn = findFollowBtn()
+    if (followBtn != null) {
+        clickView(followBtn)
+        sleep(500)
+        if (addFollow(followAccount)) {
+            console.log("关注用户:" + followAccount.account)
+        } else {
+            console.warn("关注失败")
+            clickView(followBtn)
+        }
+    } else {
+        console.warn("无效用户:" + followAccount.account)
+    }
+
+}
+
+function findFollowBtn() {
+    sleep(2000)
+    // 查找关注按钮
+    var followList = desc("关注按钮").find();
+    // 已经关注的按钮
+    var followedList = desc("已关注按钮").find();
+    console.log("找到关注按钮：" + followList.length + "--已关注：" + followedList.length)
+    if (followList.length == 0 && followedList.length == 0) {
+        sleep(1000)
+        followList = desc("关注按钮").find();
+    }
+    if (followList.length > 0) {
+        return followList[0]
+    }
+    return null
+}
+
 /**
  * 检查当前账户是否可以被关注
  * @param account
  */
-function check(account) {
+function check(followAccount) {
     var url = host + "followAccount/followCheck";
-    var data = JSON.stringify({
-        "userId": account.userId,
-        "followType": account.followType
-    });
+    var data = {
+        "userId": followAccount.userId,
+        "followType": followAccount.followType
+    };
     var resp = postRequst(url, data)
+    if (resp == true) {
+        return true
+    } else {
+        return false
+    }
+}
 
+/**
+ * 添加关注记录
+ * @param account
+ * @returns {boolean}
+ */
+function addFollow(followAccount) {
+    var url = host + "follow/add";
+    var data = {
+        "followUserId": followAccount.userId,
+        "followType": followAccount.followType,
+        "followAccount": followAccount.account
+    };
+    var resp = postRequst(url, data)
+    if (resp != null) {
+        return true
+    } else {
+        return false
+    }
 }
 
 /**
@@ -75,11 +149,29 @@ function postRequst(url, data) {
         body: data
     };
     // 发送POST请求
-    var res = http.post(url,data, options)
+    var res = http.postJson(url, data, options)
     var result = res.body.string();
-    console.log(result);
-    return result
+    var resultObject = JSON.parse(result);
+    if (resultObject.code == 200) {
+        return resultObject.data
+    } else {
+        toast(resultObject.message)
+        return null
+    }
 
+}
+
+function clickView(view) {
+    if (view == null) {
+        console.log("view未空，不能点击")
+        return
+    }
+    var bounds = view.bounds()
+    // console.log(view.bounds())
+    var centerX = bounds.left + (bounds.right - bounds.left) / 2
+    var centerY = bounds.top + (bounds.bottom - bounds.top) / 2
+    // console.log("搜索按钮中心：" + centerX + "," + centerY)
+    click(centerX, centerY)
 }
 
 
